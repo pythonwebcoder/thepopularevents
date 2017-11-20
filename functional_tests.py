4426c734fd195db0cdfd4fc6ac58813d16373313
= <=>
2017-11-19 19:44:08 -0800

    first FT specced out in comments, and now uses unittest.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,28 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://django.dev:8000/')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_get_calendar_of_popular_events(self):
+        # Kevin hears about a faster way to find popular Meetup.com events
+        # he goes to thepopularevents.com
+        self.browser.get('http://django.dev:8000')
+
+        # He notices that the page mentions The Popular Events
+        self.assertIn('The Popular Events', self.browser.title)
+        self.fail('Finish the test!')
+
+        # A calendar, that starts that day, with up to 3 most popular events per
+        # day is shown. An input box for city is filled in. The dropdown for
+        # miles radius is set to 25 miles. The input box for search is empty.
+
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
