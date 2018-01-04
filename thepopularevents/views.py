be62979092fe1487f146d96d337e350d5cf1dd0b
Tim Clemans <=>
2018-01-03 20:25:51 -0800

    moved the event data retrivial to its own recursive function

--- thepopularevents/views.py
+++ thepopularevents/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse, JsonResponse
 from . import settings
 import requests
-
+import datetime
 
 def home_page(request):
     return render(request, 'index.html', {'googleapikey': settings.GOOGLEAPIKEY, 'google_analytics_key': settings.GOOGLE_ANALYTICS_KEY})
@@ -25,32 +25,30 @@ def make_days_to_events(events):
     return dict([(date, sorted([event for event in events if convert_to_date(event.get('time')) == date], key=lambda t: t.get('yes_rsvp_count'), reverse=True))
                           for date in set([convert_to_date(row.get("time")) for row in events])])
 
-def get_events(request):
-    import requests
-    if not settings.MEETUPAPIKEY:
-        return JsonResponse({'success': False, 'error': 'MEETUPAPIKEY environment variable not set'})
-    events = []
-    import datetime
-    events_url = 'https://api.meetup.com/2/open_events?fields=timezone&and_text=False&sign=True&format=json&page=1000&radius=%s&order=time&lat=%s&lon=%s&key=%s&text_format=plain&time=,2m&text=%s' % (
-        request.GET.get('radius'), request.GET.get('lat'), request.GET.get('lng'), settings.MEETUPAPIKEY, request.GET.get('search_terms', ''))
+def get_meetup_event_data(request, next_url=None, events=[]):
+    if next_url:
+        events_url = next_url
+    else:
+        events_url = 'https://api.meetup.com/2/open_events?fields=timezone&and_text=False&sign=True&format=json&page=1000&radius=%s&order=time&lat=%s&lon=%s&key=%s&text_format=plain&time=,2m&text=%s' % (
+            request.GET.get('radius'), request.GET.get('lat'), request.GET.get('lng'), settings.MEETUPAPIKEY, request.GET.get('search_terms', ''))
     result = requests.get(events_url)
-    #&time=0m,2m
     try:
         events.extend(result.json()['results'])
     except:
-        pass
-    i = 0
-    while True:
-        i += 1
+        return events
+    next_url = result.json()['meta'].get('next')
+    if not next_url:
+        return events
+    else:
+        return get_meetup_event_data(request, next_url=next_url, events=events)
 
-        try:
-            events_url = result.json()['meta'].get('next')
-            if not events_url:
-                break
-            result = requests.get(events_url)
-            events.extend(result.json()['results'])
-        except:
-            break
+
+def get_events(request):
+
+    if not settings.MEETUPAPIKEY:
+        return JsonResponse({'success': False, 'error': 'MEETUPAPIKEY environment variable not set'})
+    events = get_meetup_event_data(request)
+
 
     top_ten_events = sorted(
         [y for y in events], key=lambda t: t.get('yes_rsvp_count'), reverse=True)[:10]
