719463e3c9a8c8d5a130ee87f762aa9954cc05be
Tim Clemans <=>
2018-01-04 23:04:24 -0800

    add rollbar

---
+++ scripts/rollbar_deploy_alert.sh
@@ -0,0 +1,9 @@
+ENVIRONMENT=production
+LOCAL_USERNAME=`whoami`
+REVISION=`git log -n 1 --pretty=format:"%H"`
+
+curl https://api.rollbar.com/api/1/deploy/ \
+  -F access_token=$ROLLBAR_DEPLOY_ACCESS_TOKEN \
+  -F environment=$ENVIRONMENT \
+  -F revision=$REVISION \
+  -F local_username=$LOCAL_USERNAME
