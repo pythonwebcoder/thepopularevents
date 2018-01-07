78ac14d87cf8b3c8a217c242f0adf4d24d10a30e
Tim Clemans <=>
2018-01-07 00:59:48 -0800

    Add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# The Popular Events
+> Lists top ten events based on location and topic search and the number of
+> reservations each event has. Also has a "must have food" filter.
+
+See https://events.pythonwebcoder.com
+
+![](screenshot1.png)
+![](screenshot2.png)
--- scripts/rollbar_deploy_alert.sh
+++ scripts/rollbar_deploy_alert.sh
@@ -1,7 +1,7 @@
 ENVIRONMENT=production
 LOCAL_USERNAME=`whoami`
 REVISION=`git log -n 1 --pretty=format:"%H"`
-
+echo $ROLLBAR_DEPLOY_ACCESS_TOKEN
 curl https://api.rollbar.com/api/1/deploy/ \
   -F access_token=$ROLLBAR_DEPLOY_ACCESS_TOKEN \
   -F environment=$ENVIRONMENT \
